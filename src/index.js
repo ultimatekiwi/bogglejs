df1dae3c2304867f1080babdf46a2d86dd3ec884
Ben Corner <bcorner@reed.edu>
2020-10-16 20:44:09 -0500

    post-darko

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import "./index.css";
 
 const cubes = [ 'AAEEGN', 'ELRTTY', 'AOOTTW', 'ABBJOO', 'EHRTVW', 'CIMOTU', 'DISTTY', 'EIOSST', 'DELRVY', 'ACHOPS', 'HIMNQU', 'EEINSU', 'EEGHNW', 'AFFKPS', 'HLNNRZ', 'DEILRX' ];
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
+// function Square(props) {
+//   return (
+//     <button className="square" onClick={props.onClick}>
+//       {props.value}
+//     </button>
+//   );
+// }
 
 class Board extends React.Component {
   constructor(props) {
@@ -22,85 +22,113 @@ class Board extends React.Component {
   }
 
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = cubes[i][Math.floor(Math.random()*6)];  //6 for the possible faces on that cube
-//    squares[i] = 'X';
-//    this.setState({squares: squares});
-    this.setState(   {squares: squares});  
-  }
+//   handleClick(i) {
+//     const squares = this.state.squares.slice();
+//     squares[i] = cubes[i][Math.floor(Math.random()*6)];  //6 for the possible faces on that cube
+// //    squares[i] = 'X';
+// //    this.setState({squares: squares});
+//     this.setState(   {squares: squares});  
+//   }
 
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
+//   renderSquare(i) {
+//     return (
+//       <Square
+//         value={this.state.squares[i]}
+//         onClick={() => this.handleClick(i)}
+//       />
+//     );
+//   }
 
-/*
     render() {
     const status = 'Next player: X';
+    const { boardState } = this.props;
+
+    // What is happening in the next 10 lines? Fuck me if I can explain it.
+    const boardRows = boardState.map((row) => (
+      <div className="board-row">
+        {row.map((square) => (
+          <span className="square">{square}</span>
+        ))}
+      </div>
+    ))
+    
+// so this boardRows variable is really an array of 4 React.Div objects,
+// each of them having 4 React.Span children
+
     
     return (
         <div>
         <div className="status">{status}</div>
-          <div className="board-row"></div>
-           {cubes.map(cube => (
-             <li key={cube}>
-               {this.renderSquare(cube)}
-             </li>
-           ))
-           }
+          {boardRows}
         </div>
     );
   }
- */
 
 
 // lol fuck it, copy-paste for the moment.
-  render() {
-    const status = 'Next player: X';
+  // render() {
+  //   const status = 'Next player: X';
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          {this.renderSquare(3)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
-          {this.renderSquare(10)}
-          {this.renderSquare(11)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(12)}
-          {this.renderSquare(13)}
-          {this.renderSquare(14)}
-          {this.renderSquare(15)}
-        </div>
-      </div>
-    );
-  }
+  //   return (
+  //     <div>
+  //       <div className="status">{status}</div>
+  //       <div className="board-row">
+  //         {this.renderSquare(0)}
+  //         {this.renderSquare(1)}
+  //         {this.renderSquare(2)}
+  //         {this.renderSquare(3)}
+  //       </div>
+  //       <div className="board-row">
+  //         {this.renderSquare(4)}
+  //         {this.renderSquare(5)}
+  //         {this.renderSquare(6)}
+  //         {this.renderSquare(7)}
+  //       </div>
+  //       <div className="board-row">
+  //         {this.renderSquare(8)}
+  //         {this.renderSquare(9)}
+  //         {this.renderSquare(10)}
+  //         {this.renderSquare(11)}
+  //       </div>
+  //       <div className="board-row">
+  //         {this.renderSquare(12)}
+  //         {this.renderSquare(13)}
+  //         {this.renderSquare(14)}
+  //         {this.renderSquare(15)}
+  //       </div>
+  //     </div>
+  //   );
+  // }
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boardState: []
+    }
+  }
+
+  shuffleBoard() {
+    // TODO: generate a random 4x4 board here
+    // const newBoard = [];
+
+
+    const newBoard = [['M', 'A', 'R', 'C'], ['U', 'S', '=', 'L'], ['E', 'X', 'L', 'U'], ['T', 'H', 'O', 'R']]
+    this.setState({
+      boardState: newBoard
+    })
+  }
+
+  componentDidMount() {
+    this.shuffleBoard()
+  }
+
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board boardState={this.state.boardState}/>
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
